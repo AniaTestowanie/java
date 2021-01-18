739f30baafe52ba5b0ab98339af1290f6f20fe01
Ania <anna.m.szmidt@gmail.com>
2021-01-18 10:52:32 +0100

    Aktualizacja gitignore

---
+++ src/InstrukcjaWarunkowaIf.java
@@ -0,0 +1,7 @@
+public class InstrukcjaWarunkowaIf {
+// Instrukcje warunkowe pozwalają nam określać które części kodu mają zostać wykonane,gdy zostanie spełniony warunek
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/OperatoryLogiczne.java
@@ -0,0 +1,18 @@
+public class OperatoryLogiczne {
+    // Działają na wartościach logicznych zwracając również wartości logiczne
+
+    public static void main(String[] args) {
+        boolean firstValue = true;
+        boolean secondValue = false;
+        boolean thirdValue = true;
+        boolean fourthValue = false;
+
+        System.out.println(firstValue && secondValue);
+        System.out.println(firstValue && thirdValue);
+        System.out.println(firstValue || secondValue);
+        System.out.println(secondValue || fourthValue);
+        System.out.println(firstValue);
+        System.out.println(secondValue);
+        System.out.println(firstValue && secondValue);
+    }
+}
